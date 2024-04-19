02faca59af1147ead8b2fb6868f5c8d7109bff54
Alexander Ivanov <alex.p.ivanov@gmail.com>
2024-04-19 12:53:35 +0300

    Updated upx to 4.2.3

--- upx/install.sh
+++ upx/install.sh
@@ -9,11 +9,18 @@ else
 fi
 
 upx_bin=upx
-upx_ver=${1:-"4.2.2"}
+upx_ver=${1:-"4.2.3"}
 upx_dir=upx-$upx_ver-$target
 upx_ext=tar.xz
 upx_zip=$upx_dir.$upx_ext
 upx_uri=$GITHUB/upx/upx/releases/download/v$upx_ver/$upx_zip
+
+
+# https://github.com/upx/upx
+# wget "https://github.com/upx/upx/releases/download/v4.2.3/upx-4.2.3-amd64_linux.tar.xz"
+# tar x -f "upx-4.2.3-amd64_linux.tar.xz"
+# mv "upx-4.2.3-amd64_linux/upx" "/usr/local/bin/."
+# rm -r "upx-4.2.3-amd64_linux.tar.xz" "upx-4.2.3-amd64_linux"
 
 
 # if [ $(ls /usr/local/bin | grep -c "$upx_bin") -eq 0 ]; then
